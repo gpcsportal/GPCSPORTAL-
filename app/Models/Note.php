b0eddc0685c58341c496f19db96ba6726e8372c9
gpcsportal <gpcsportal@gmail.com>
2026-09-17 20:50:47 +0530

    Allow notes file hash persistence

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -1,4 +1,32 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
-class Note extends Model {protected $fillable=['user_id','branch','semester','year','subject_name','subject_code','title','description','attachment_path','original_name','mime_type','file_size','fingerprint','status','rejection_reason']; public function user(){return $this->belongsTo(User::class);} }
+
+class Note extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'branch',
+        'semester',
+        'year',
+        'subject_name',
+        'subject_code',
+        'title',
+        'description',
+        'attachment_path',
+        'original_name',
+        'mime_type',
+        'file_size',
+        'file_hash',
+        'fingerprint',
+        'status',
+        'rejection_reason',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}

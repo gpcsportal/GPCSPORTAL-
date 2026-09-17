c7a3048a130ed11ef9faa3c0f5b7757786be711f
gpcsportal <gpcsportal@gmail.com>
2026-09-17 17:47:29 +0530

    Normalize production APP_URL to HTTPS

--- config/app.php
+++ config/app.php
@@ -1,11 +1,35 @@
 <?php
+
+$environment = (string) env('APP_ENV', 'production');
+$railwayDomain = trim((string) env('RAILWAY_PUBLIC_DOMAIN', ''));
+$defaultUrl = $railwayDomain !== ''
+    ? 'https://'.$railwayDomain
+    : 'https://gpcsportal.up.railway.app';
+
+$appUrl = trim((string) env('APP_URL', $defaultUrl));
+
+if ($environment === 'production') {
+    if (str_starts_with($appUrl, 'http://')) {
+        $appUrl = 'https://'.substr($appUrl, 7);
+    } elseif (! preg_match('#^https://#i', $appUrl)) {
+        $appUrl = 'https://'.ltrim($appUrl, '/');
+    }
+}
+
 return [
     'name' => env('APP_NAME', 'GPCS Portal'),
-    'env' => env('APP_ENV', 'production'),
+    'env' => $environment,
     'debug' => (bool) env('APP_DEBUG', false),
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => rtrim($appUrl, '/'),
     'timezone' => env('APP_TIMEZONE', 'Asia/Kolkata'),
-    'locale' => 'en', 'fallback_locale' => 'en', 'faker_locale' => 'en_IN',
-    'cipher' => 'AES-256-CBC', 'key' => env('APP_KEY'), 'previous_keys' => [],
-    'maintenance' => ['driver' => 'file', 'store' => 'database'],
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'en_IN',
+    'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY'),
+    'previous_keys' => [],
+    'maintenance' => [
+        'driver' => 'file',
+        'store' => 'database',
+    ],
 ];

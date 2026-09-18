5723df9fcc500864ceb3a059c83cb6dfbfe483c4
gpcsportal <gpcsportal@gmail.com>
2026-09-18 21:07:36 +0530

    Delete Digital Board notification model

--- app/Models/PortalNotification.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class PortalNotification extends Model {protected $fillable=['admin_id','audience','recipient','title','message','link']; public function admin(){return $this->belongsTo(User::class,'admin_id');}}
